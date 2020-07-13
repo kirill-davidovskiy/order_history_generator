6ed4d4bd7f02b232430b89f2494921ede6cfeb67
Smith <kirill.davidovskiy.work@gmail.com>
2020-07-14 00:06:25 +0300

    logger and config

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-import datetime
+from datetime import datetime,timedelta
 import json
 import logging
+import configparser
+import os
 
 
 basic_generator, set_of_var  = {'a': 84589, 'c': 45989, 'm': 217728}, {}
 order_history, providers, directions, currency_pairs = [], [], [], []
 number_of_orders = 0
+start_date = datetime.now()
+PATH_LOGS = os.getcwd()+"/Logs/"
+PATH_DATABASE = os.getcwd()+"/database.json"
+logging.basicConfig(filename = PATH_LOGS + datetime.datetime.now().strftime("%d:%m:%Y-%H:%M"), level = logging.INFO)
 
+def create_config(path):
+	config = configparser.ConfigParser()
+	config.add_section("PATH")
+	config.set("PATH", "Logs", PATH_LOGS)
+	config.set("PATH", "DataBase", PATH_DATABASE)
+	with open(path, "w") as conf_file:
+		config.write(conf_file)
 
-def get_config():
-    global number_of_orders, set_of_var, providers, directions, currency_pairs
+def read_config():
+    logging.info("Reading config")
+    if not os.path.exists(path):
+        logging.warn("CANT FIND CONFIG FILE, CREATING STANDART AND USING IT")
+        create_config(path)
+    else:
+        config = configparser.ConfigParser()
+        config.read(path)
+        PATH_LOGS = config.get("PATH", "Logs")
+        PATH_DATABASE = config.get("PATH", "DataBase")
+    global number_of_orders, set_of_var, providers, directions, currency_pairs, start_date
     with open('config.json') as config_file:
         data = json.load(config_file)
-    print("Type:", type(data))
     number_of_orders = data['number_of_orders']
     set_of_var = data['set_of_var']
     providers = data['providers']
     directions = data['directions']
     currency_pairs = list(data['currency_pairs'].items())
+    start_date = datetime.strptime(data['start_date'], '%d/%m/%y %H:%M:%S')
 
 
 def init():
@@ -39,7 +61,6 @@ def update_generator_vars():
     set_of_var['a'] = generator_rnd_number(basic_generator, set_of_var['a'])
     set_of_var['c'] = generator_rnd_number(basic_generator, set_of_var['c'])
     set_of_var['m'] = generator_rnd_number(basic_generator, set_of_var['m'])
-    print(set_of_var)
 
 
 def generate_id():
@@ -83,11 +104,14 @@ def generate_currency():
 
 
 def generate_dates():
-    global order_history, set_of_var
-    currentDate = datetime.datetime.now()
-    currentDate += datetime.timedelta(microseconds = 100500)
+    global order_history, set_of_var, start_date
+    prev = 18603
+    prev_date = start_date
     for order in order_history:
-        order.append(generator_from_template(set_of_var, order[0], currency_pairs))
+        prev = prev + generator_rnd_number(set_of_var, prev) * 15000 + 1
+        current_date = prev_date + timedelta(microseconds=prev)
+        #order.append(current_date.isoformat(sep='|'))
+        order.append(current_date)
 
 
 def main():
@@ -96,6 +120,7 @@ def main():
     generate_provider()
     generate_direction()
     generate_currency()
+    generate_dates()
     for order in order_history:
         print(order)
 
